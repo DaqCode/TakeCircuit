3ae84d3dbfdf72cc6667802ce45f9b3b2f0d330a
Daq_Vid <dlim0012.gaming@gmail.com>
2025-02-01 12:58:06 -0800

    Reset, calling it Take Poison

--- new-game-project/event_bus.gd
+++
@@ -1,7 +0,0 @@
-class_name Global
-extends Node
-
-signal battery_collected
-
-func emit_battery_collected():
-	emit_signal("battery_collected")
--- new-game-project/scenes/pickUps/battery/battery.gd
+++
@@ -1,8 +0,0 @@
-class_name Battery
-extends Area2D
-
-
-func _on_battery_entered(body:Node2D) -> void:
-	if body.name == "Spark":
-		EventBus.emit_battery_collected()
-		queue_free()
